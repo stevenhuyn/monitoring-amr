4d29fa6c0e976aeff8c95be7757ddc5ced82d6b0
Exodus26 <caleb2gizmo@gmail.com>
2024-04-23 13:59:26 +0200

    fixed output_csv.py

--- tool/output_csv.py
+++ tool/output_csv.py
@@ -1,32 +1,20 @@
 import csv
 import os
 
-NEW_DATA = 'outputs/new_monitoring_amr.csv'
-OLD_DATA = 'outputs/monitoring_amr.csv'
-
-def check_new(data):
-    pass
-
-def process_results(data):
-    for i,result in enumerate(data):
-        del result.text
-        # if result.GPT_response == '':
-        #     data.pop(i)
-            
-def process_prompt(data):
-    pass
+NEW_DATA = 'new_monitoring_amr.csv'
+OLD_DATA = 'monitoring_amr.csv'
 
 def write_to_csv(data):
-    process_results(data)
     # Check if the CSV file already exists
     file_exists = os.path.exists(OLD_DATA)
+    
 
     # Get fieldnames from the class attributes of the search_result object
     fieldnames = data[0].__dict__.keys()
 
     # Write to the old data file
-    with open(OLD_DATA, 'a', newline='', encoding='utf-8') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    with open(os.path.join('tool','outputs',OLD_DATA), 'a', newline='', encoding='utf-8',) as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';;')
 
         # Write data to CSV
         for result in data:
@@ -34,8 +22,8 @@ def write_to_csv(data):
 
     # If the old data file doesn't exist, create it and write the header
     if not file_exists:
-        with open(OLD_DATA, 'w', newline='', encoding='utf-8') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        with open(os.path.join('tool','outputs',OLD_DATA), 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=';;')
             writer.writeheader()
 
             # Write data to CSV
@@ -43,10 +31,11 @@ def write_to_csv(data):
                 writer.writerow(row.__dict__)
 
     # Always overwrite the new data file
-    with open(NEW_DATA, 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    with open(os.path.join('tool','outputs',NEW_DATA), 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=';;')
         writer.writeheader()
 
         # Write data to CSV
         for row in data:
             writer.writerow(row.__dict__)
+
