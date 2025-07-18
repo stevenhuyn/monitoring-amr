11adb8be53a319ae8a0bf18420ddf76badc7d19f
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2025-07-18 15:38:18 +1000

    Fix path for prompts

--- leximine/src/app/__init__.py
+++ leximine/src/app/__init__.py
@@ -9,15 +9,15 @@ def main():
     articlePages = scraper.scrapeGoogle(["antibiotics resistance"])
 
     systemPrompt = None
-    with open("./packages/my_lib/src/my_lib/prompts/system.txt", "r") as f:
+    with open("./packages/core/src/core/prompts/system.txt", "r") as f:
         systemPrompt = f.read()
 
     filterPrompt = None
-    with open("./packages/my_lib/src/my_lib/prompts/filter.txt", "r") as f:
+    with open("./packages/core/src/core/prompts/filter.txt", "r") as f:
         filterPrompt = f.read()
 
     extractPrompt = None
-    with open("./packages/my_lib/src/my_lib/prompts/extract.txt", "r") as f:
+    with open("./packages/core/src/core/prompts/extract.txt", "r") as f:
         extractPrompt = f.read()
 
     extractor = Extractor(systemPrompt, extractPrompt, filterPrompt)
