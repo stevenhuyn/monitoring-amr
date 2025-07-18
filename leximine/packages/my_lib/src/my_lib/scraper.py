a862e49e0711fc691ebc9d7b3a404977b69a0d66
stevenhuyn <18359644+stevenhuyn@users.noreply.github.com>
2025-07-18 11:13:49 +1000

    Add basic POC for search google news

--- leximine/packages/my_lib/src/my_lib/scraper.py
+++ leximine/packages/my_lib/src/my_lib/scraper.py
@@ -11,33 +11,57 @@ class SeleniumScraper:
         self.driver.set_page_load_timeout(5)
 
     def scrapeGoogle(self, queries: List[str]):
+        articleResults = []
         for query in queries:
-            searchParams = {
-                # Interface Language
-                "hl": "en-IN",
-                # Country of the document
-                "gl": "IN",
-                "ceid": "IN:en",
-                "q": query,
-            }
-            # ?hl=en-IN&gl=IN&ceid=IN%3Aen
-            scheme, netloc, path, queryString, fragment = urlsplit(
-                "https://news.google.com/search?dog=4"
-            )
-            queryParams = parse_qs(queryString)
-            queryParams.update(searchParams)
-            newQueryString = urlencode(queryParams)
-
-            searchUrl = urlunsplit((scheme, netloc, path, newQueryString, fragment))
-
+            searchUrl = SeleniumScraper.buildSearchUrl(query)
+            print(searchUrl)
             self.driver.get(searchUrl)
-
-            time.sleep(10)
+            time.sleep(5)
 
             search_results = self.driver.find_elements(
                 By.XPATH, '//div[@class="m5k28"]'
             )
 
-            print(search_results)
+            for result in search_results:
+                frontPage = result.find_element(By.XPATH, './/div[@class="B6pJDd"]')
+                title = frontPage.text
+                articleUrl = frontPage.find_element(By.CSS_SELECTOR, "a").get_attribute(
+                    "href"
+                )
+                articleResult = ArticleResult(query, articleUrl, title)
+                articleResults.append(articleResult)
 
-            self.driver.quit()
+        self.driver.quit()
+
+        for articleResult in articleResults:
+            print(articleResult)
+
+    def buildSearchUrl(query: str) -> str:
+        searchParams = {
+            # Interface Language
+            "hl": "en-IN",
+            # Country of the document
+            "gl": "IN",
+            "ceid": "IN:en",
+            "q": query,
+        }
+        # ?hl=en-IN&gl=IN&ceid=IN%3Aen
+        scheme, netloc, path, queryString, fragment = urlsplit(
+            "https://news.google.com/search?dog=4"
+        )
+        queryParams = parse_qs(queryString)
+        queryParams.update(searchParams)
+        newQueryString = urlencode(queryParams)
+
+        searchUrl = urlunsplit((scheme, netloc, path, newQueryString, fragment))
+        return searchUrl
+
+
+class ArticleResult:
+    def __init__(self, query, url, title):
+        self.query = query
+        self.url = url
+        self.title = title
+
+    def __repr__(self):
+        return str((self.query, self.url))
